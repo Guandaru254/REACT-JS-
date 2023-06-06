3e7d6c0e4321113af3e52d5aa570418ec2019914
Guandaru254 <leroykim82@gmail.com>
2023-06-06 16:02:50 +0300

    Add files via upload

---
+++ accessstate.js
@@ -0,0 +1,15 @@
+const MyComponent = () => { 
+const [count, setCount] = useState (0);
+
+const incrementCount = () => {
+    setCount(count + 1);
+  };
+
+return
+ (    
+<div>     
+<p>Count: {count}</p>     
+<button onClick = {incrementCount}>Increment</button>    
+</div>
+  );
+};
