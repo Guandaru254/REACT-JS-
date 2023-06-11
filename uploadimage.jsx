6be8c38a01c7a520233d6fecf14cf5cdc916c6d4
Guandaru254 <leroykim82@gmail.com>
2023-06-11 21:51:28 +0300

    Add files via upload

---
+++ uploadimage.jsx
@@ -0,0 +1,23 @@
+const scrollX = useRef(new Animated.Value(0)).current;
+ 
+ 
+<ScrollView
+        scrollEnabled={enableScroll}
+        horizontal={true}
+        pagingEnabled
+        showsHorizontalScrollIndicator={false}
+        onScroll={Animated.event(
+          [
+            {
+              nativeEvent: {
+                contentOffset: {
+                  x: scrollX,
+                },
+              },
+            },
+          ],
+          { useNativeDriver: false },
+        )}
+        scrollEventThrottle={1}>
+
+</ScrollView>
